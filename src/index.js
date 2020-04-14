92b4d4f971d4eaac8c174164c12e8c333a2cb4b2
guolei <guolei@troila.com>
2020-04-14 14:44:01 +0800

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      hellos123221321
+    </div>
+  );
+};
+
+render(<App/>, document.getElementById('root'));
